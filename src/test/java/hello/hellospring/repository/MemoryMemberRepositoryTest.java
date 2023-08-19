fb082edee9e478a9c68eebf073201cec1a938cfb
devpigKing <devpigking@gmail.com>
2023-08-20 01:59:17 +0900

    #코드로 배우는 스프링 부트 공부입니다.

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,2 +1,8 @@
-package hello.hellospring.repository;public class MemoryMemberRepositoryTest {
+package hello.hellospring.repository;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+
+
 }
